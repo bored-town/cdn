451cf2afbb09c9841b742d119352bcdb2c78558c
diewland <diewland@gmail.com>
2023-09-16 15:38:21 +0700

    rename scripts

---
+++ root.js
@@ -0,0 +1,20 @@
+const { ethers } = require('ethers')
+const { MerkleTree } = require('merkletreejs')
+const { keccak256 } = ethers//.utils
+const fs = require('fs')
+
+const args = process.argv.slice(2)
+let csv_path = args[0]
+
+fs.readFile(csv_path, 'utf8', (err, data) => {
+
+  // load wl from file
+  let whitelist = data.split('\n').map(r => r.split(',')[0].trim())
+
+  // get tree
+  let leaves = whitelist.map((addr) => keccak256(addr))
+  let tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
+  let root = tree.getHexRoot()
+  console.log('root: ' + root)
+
+})
