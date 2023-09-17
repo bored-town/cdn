ac193da4d64cce2ef662c2e6e4fe4cefb413517d
diewland <diewland@gmail.com>
2023-09-17 21:27:48 +0700

    remove empty row in csv

--- proof.js
+++ proof.js
@@ -9,14 +9,15 @@ let out_dir = args[1]
 
 fs.readFile(csv_path, 'utf8', (err, data) => {
 
-  // load wl from file
-  let whitelist = data.split('\n').map(r => r.split(',')[0].trim())
+  // load wl from file, filter for remove last empty row
+  let whitelist = data.split('\n').map(r => r.split(',')[0].trim()).filter(r => r)
+  console.log('total:', whitelist.length)
 
   // get tree
   let leaves = whitelist.map((addr) => keccak256(addr))
   let tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
   let root = tree.getHexRoot()
-  console.log('root: ' + root)
+  console.log('root:', root)
 
   // separate wl by first char after 0x
   let chunk = {}
@@ -32,7 +33,7 @@ fs.readFile(csv_path, 'utf8', (err, data) => {
     chunk[s][l] = proof
   })
   let usage_time = (new Date()) - from_time
-  console.log('usage time: ' + (usage_time / 1_000) + 's')
+  console.log('usage time:', usage_time/1_000, 's')
 
   // debug
   // let sum = 0
--- root.js
+++ root.js
@@ -8,13 +8,14 @@ let csv_path = args[0]
 
 fs.readFile(csv_path, 'utf8', (err, data) => {
 
-  // load wl from file
-  let whitelist = data.split('\n').map(r => r.split(',')[0].trim())
+  // load wl from file, filter for remove last empty row
+  let whitelist = data.split('\n').map(r => r.split(',')[0].trim()).filter(r => r)
+  console.log('total:', whitelist.length)
 
   // get tree
   let leaves = whitelist.map((addr) => keccak256(addr))
   let tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
   let root = tree.getHexRoot()
-  console.log('root: ' + root)
+  console.log('root:', root)
 
 })
