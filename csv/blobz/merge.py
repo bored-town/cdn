f2413f1c8c5884d2d180cc812bdffeb82a7947f5
diewland <diewland@gmail.com>
2024-03-12 23:07:36 +0700

    fix wally zksync snapshot

--- csv/blobz/merge.py
+++ csv/blobz/merge.py
@@ -3,11 +3,13 @@ from pprint import pprint as pp
 
 csv_bt       = './bt_full.csv'
 csv_tripster = './tripster.csv'
+csv_wally    = './wally_zksync.csv' # fix zksync snap not complete
 
 addr_bt       = [ line.strip().split(',')[0].lower() for line in open(csv_bt, 'r') ]
 addr_tripster = [ line.strip().lower() for line in open(csv_tripster, 'r') ]
+addr_wally    = [ line.strip().split(',')[0].lower() for line in open(csv_wally, 'r') ]
 
-wl = set(addr_bt + addr_tripster)
+wl = set(addr_bt + addr_tripster + addr_wally)
 
 for w in sorted(wl):
     print("{},0".format(to_checksum_address(w)))
